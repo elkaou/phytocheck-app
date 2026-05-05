b63bb5a153ee85e18dee338314ed0e75b01d03c2
elkaou <francois@siteswebs.fr>
2026-05-05 11:36:02 +0200

    Version 1.1.2

--- app.config.ts
+++ app.config.ts
@@ -41,7 +41,7 @@ const env = {
 const config: ExpoConfig = {
   name: env.appName,
   slug: env.appSlug,
-  version: "1.1.1",
+  version: "1.1.2",
   // Pas de restriction d'orientation pour compatibilité grand écran (Android 16+)
   orientation: "default",
   icon: "./assets/images/icon.png",
@@ -51,7 +51,6 @@ const config: ExpoConfig = {
   ios: {
     supportsTablet: true,
     bundleIdentifier: env.iosBundleId,
-    buildNumber: "84",
     infoPlist: {
       ITSAppUsesNonExemptEncryption: false,
       CFBundleDevelopmentRegion: "fr",
@@ -59,7 +58,6 @@ const config: ExpoConfig = {
     },
   },
   android: {
-    versionCode: 83,
     adaptiveIcon: {
       backgroundColor: "#E6F4FE",
       foregroundImage: "./assets/images/android-icon-foreground.png",
