d06b4ea7f761a2ac988a6d75cf0bdb76c093ce54
elkaou <francois@siteswebs.fr>
2026-05-06 16:09:53 +0200

    v1.1.5 : fix filtre culture UsagesModal avec alias (Blé -> Céréales à paille)

--- app.config.ts
+++ app.config.ts
@@ -41,7 +41,7 @@ const env = {
 const config: ExpoConfig = {
   name: env.appName,
   slug: env.appSlug,
-  version: "1.1.4",
+  version: "1.1.5",
   // Pas de restriction d'orientation pour compatibilité grand écran (Android 16+)
   orientation: "default",
   icon: "./assets/images/icon.png",
