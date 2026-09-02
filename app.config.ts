efa7bc568543a0fdca755cca98903cdd799413fe
elkaou <2.58649616e+08+elkaou@users.noreply.github.com>
2026-09-02 15:05:33 +0000

    release: version 1.1.11 avec R8 Android

--- app.config.ts
+++ app.config.ts
@@ -41,7 +41,7 @@ const env = {
 const config: ExpoConfig = {
   name: env.appName,
   slug: env.appSlug,
-  version: "1.1.10",
+  version: "1.1.11",
   // Pas de restriction d'orientation pour compatibilité grand écran (Android 16+)
   orientation: "default",
   icon: "./assets/images/icon.png",
